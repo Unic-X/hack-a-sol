8de36ad8231f9d170055227be3e235bfc84dbba3
KHUSHDEEP-17 <kdsingh420840@gmail.com>
2024-09-14 06:55:51 +0530

    Player UI upgrade

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,42 @@
+// App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import PlayerInfo from './PlayerInfo'; // Import new PlayerInfo page
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PlayerList from './Players';
+import PlayerDetail from './components/PlayerDetail';
 
-function App() {
-  const players = [
-    { id: 1, name: 'Player 1', sport: 'Basketball', position: 'Forward' },
-    { id: 2, name: 'Player 2', sport: 'Football', position: 'Goalkeeper' },
-    { id: 3, name: 'Player 3', sport: 'Tennis', position: 'Single' },
-    { id: 5, name: 'Player 1', sport: 'Basketball', position: 'Forward' },
-    { id: 7, name: 'Player 1', sport: 'Basketball', position: 'Forward' },
-    { id: 9, name: 'Player 1', sport: 'Basketball', position: 'Forward' },
+const playerData = {
+  players: [
+    {
+      playerId: 1,
+      name: "Virat Kohli",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/15/Virat_Kohli_portrait.jpg",
+      bio: "Indian cricketer and former captain of the Indian national team."
+    },
+    {
+      playerId: 2,
+      name: "MS Dhoni",
+      image: "https://upload.wikimedia.org/wikipedia/commons/c/c9/MS_Dhoni.jpg",
+      bio: "Former captain of the Indian national team and a successful wicket-keeper."
+    },
+    {
+      playerId: 3,
+      name: "Rohit Sharma",
+      image: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Rohit_Gurunath_Sharma.jpg",
+      bio: "Indian cricketer who plays as an opening batsman."
+    },
+    // Add more players as needed
+  ],
+};
 
-  ];
-
+const App = () => {
   return (
     <Router>
-      <div className="player-container">
-        <Routes>
-          {/* Home page */}
-          <Route
-            path="/"
-            element={
-              <>
-                <div className="search-bar">
-                  <input type="text" placeholder="Search Players" />
-                </div>
-                <div className="player-grid">
-                  {players.map((player) => (
-                    <Link to={`/player/${player.id}`} key={player.id} className="player-card">
-                      <div className="player-image"></div>
-                      <div className="player-name">{player.name}</div>
-                      <div className="player-tags">
-                        <span className="tag">{player.sport}</span>
-                        <span className="tag">{player.position}</span>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              </>
-            }
-          />
-          {/* Player info page */}
-          <Route path="/player/:id" element={<PlayerInfo players={players} />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<PlayerList players={playerData.players} />} />
+        <Route path="/player/:id" element={<PlayerDetail players={playerData.players} />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
