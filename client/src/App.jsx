f5bb38703de93e61f61a8af10661b07f51356175
Unic-X <armansinghkshatri69@gmail.com>
2024-09-15 05:53:21 +0530

    Lesse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
   const fetchPlayers = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`http://localhost:5000/api/players?limit=100&offset=${offset}`);
+      const response = await fetch(`https://loonix.in:8443/api/players?limit=100&offset=${offset}`);
       const data = await response.json();
 
       const playersWithUniqueIds = data.map((player, index) => ({
